390a65e37f51d3a8b710c7237255b08141066516
Michael Arenzon <marenzo@users.noreply.github.com>
2019-12-18 14:38:47 +0200

    adding 'Virtualhosting' level 5

--- levels/level5/README.md
+++ levels/level5/README.md
@@ -1,1 +1,21 @@
-# Level 6: Virtualhosting
+# Level 5: Virtualhosting
+One of the great improvements of HTTP/1.1 is the `Host` header.
+Using this header we can preserve the original domain the request was sent to, and in this exercise
+we'll leverage that header in order to implement [VirtualHosting](https://en.wikipedia.org/wiki/Virtual_hosting)
+
+You'll need to implement function `is_domain_allowed` and `get_domain_content` to return different responses according to the domain:  
+```
+domain1.com => "Hello Domain1"  
+domain2.com => "Hello Domain2"
+```
+
+## Exercises
+- Return HTML response only for set of allowed domains like in the example above
+- Does the `Host` header contains the server port? How we can deal that?
+- What happens if the request doesn't contains `Host` header? (Hint: [Bad Request](https://tools.ietf.org/html/rfc2616#section-14.23))
+- What happens if a request was sent with `Host` header contains unsupported domain? (Hint: [Bad Request](https://tools.ietf.org/html/rfc2616#section-5.2))
+- Bonus: how we can check it locally and simulating requests to `domain1.com` and `domain2.com` without owning the domains?
+
+# Documentation
+- [`Host` on MDN](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Host)
+- [`headers` of Python BaseHTTPRequestHandler](https://docs.python.org/3/library/http.server.html#http.server.BaseHTTPRequestHandler.headers)
--- levels/level5/server.py
+++ levels/level5/server.py
@@ -2,22 +2,45 @@
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+allowed_domains = {
+    "domain1.com": "Hello Domain1",
+    "domain2.com": "Hello Domain2"
+}
+
+
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        body = '<html><body><p>Hello world</p></body></html>'.encode('utf-8')
-        if self.path == '/':
-            pass
-            # self.wfile.write() is your friend
-        else:
-            self.send_response(404, 'Not found')
-            # after sending the status line, we send headers
+        self.route()
+
+    def route(self):
+        if self.is_domain_allowed():
+            content = self.get_domain_content()
+            body = '<html><body><p>{}</p></body></html>'.format(content).encode('utf-8')
+
+            self.send_response(200, 'OK')
+            self.send_header('Content-Type', 'text/html; charset=utf-8')
+            self.send_header('Content-Length', len(body))
             self.send_header('Server', 'demo')
             self.end_headers()
-        
+            self.wfile.write(body)
+        else:
+            # TODO: what happens if the domain is not allowed / Host header incorrect
+            raise Exception("Not implemented yet")
+
+    def is_domain_allowed(self):
+        # TODO: read Host header and match the domain
+        # Use: self.headers array to get header's value
+        # https://docs.python.org/3/library/http.server.html#http.server.BaseHTTPRequestHandler.headers
+
+        return True
+
+    def get_domain_content(self):
+        # TODO: read Host header and get the content
+        return ""
+
 
 server = HTTPServer(('', 8080), RequestHandler)
 
 if __name__ == "__main__":
     print('Listening on port 8080')
     server.serve_forever()
-
